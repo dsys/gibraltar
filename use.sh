3ea9699efd1bf634756b9b0ce179e5c9a974dcdd
Alex Kern <alex@kern.io>
2016-01-23 20:22:32 -0800

    Ignore env.sh.

--- use.sh
+++
@@ -1,3 +0,0 @@
-export PATH=`dirname `/versions/current/bin:$PATH
-export LD_LIBRARY_PATH=`dirname ASH_SOURCE`/versions/current/lib:/usr/local/cuda/lib:$LD_LIBRARY_PATH 
-export DYLD_LIBRARY_PATH=`dirname ASH_SOURCE`/versions/current/lib:/usr/local/cuda/lib:$DYLD_LIBRARY_PATH
